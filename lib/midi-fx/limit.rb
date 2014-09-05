73de2759dba16504e96cb853f724c7d971325c56
Ari Russo <ari.russo@gmail.com>
2014-09-04 21:02:16 -0400

    limit effect

---
+++ lib/midi-fx/limit.rb
@@ -0,0 +1,27 @@
+module MIDIFX
+
+  class Limit
+
+    attr_reader :property, :limit_to
+    alias_method :range, :limit_to
+
+    def initialize(property, limit_to, options = {})
+      @limit_to = limit_to
+      @property = property
+      @name = options[:name]
+    end
+
+    def process(message)
+      val = message.send(@property)
+      if @limit_to.kind_of?(Range)
+        message.send("#{@property}=", @limit_to.min) if val < @limit_to.min
+        message.send("#{@property}=", @limit_to.max) if val > @limit_to.max
+      elsif @limit_to.kind_of?(Numeric)
+        message.send("#{@property}=", @limit_to)
+      end
+      message
+    end
+
+  end
+
+end
