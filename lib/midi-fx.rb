7029e265e3a6c5f8c433cd772c195e95f05c8948
Ari Russo <ari.russo@gmail.com>
2014-09-04 21:02:34 -0400

    module

---
+++ lib/midi-fx.rb
@@ -0,0 +1,37 @@
+#
+# midi-fx
+#
+# MIDI effects in Ruby
+#
+# (c)2011-2014 Ari Russo
+# Apache 2.0 License
+# 
+
+# libs
+require "midi-message"
+
+# classes
+require "midi-fx/filter"
+require "midi-fx/limit"
+require "midi-fx/transpose"
+
+module MIDIFX
+
+  VERSION = "0.1"
+
+  MAP = {
+    :filter => Filter,
+    :limit => Limit, 
+    :transpose => Transpose
+  }
+
+  def self.method_missing(method, *args, &block)
+    if MAP.keys.include?(method)
+      message = args.shift
+      MAP[method].new(*args).process(message, &block)
+    else
+      super
+    end
+  end
+
+end
