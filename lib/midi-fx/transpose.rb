3e45369bc18eaf1bcde3385591cbb9eafc892436
Ari Russo <ari.russo@gmail.com>
2014-09-04 21:01:48 -0400

    transpose effect

---
+++ lib/midi-fx/transpose.rb
@@ -0,0 +1,21 @@
+module MIDIFX
+
+  class Transpose
+
+    attr_reader :factor, :property
+
+    def initialize(property, factor, options = {})
+      @factor = factor
+      @property = property
+      @name = options[:name]
+    end
+
+    def process(message)
+      val = message.send(@property)
+      message.send("#{@property}=", val + @factor)
+      message
+    end
+
+  end
+
+end
